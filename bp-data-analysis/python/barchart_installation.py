5cd0d25d67a4a60fe8305cd1cc8fc55462f222b3
Tijs <tijs.martens@student.hogent.be>
2020-05-25 10:08:49 +0200

    added barplot
    barplot for that shows the amount of people who would prefer a PWA over a native app

---
+++ bp-data-analysis/python/barchart_installation.py
@@ -0,0 +1,29 @@
+# libraries
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# set width of bar
+barWidth = 0.25
+ 
+# set height of bar
+bars1 = [0.355, 0.007]
+bars2 = [1, 0.25]
+
+# Set position of bar on X axis
+r1 = np.arange(2)
+r2 = [x + barWidth for x in r1]
+ 
+# Make the plot
+plt.bar(r1, bars1, color='#FF5376', width=barWidth, edgecolor='white', label='IOS')
+plt.bar(r2, bars2, color='#242154', width=barWidth, edgecolor='white', label='Android')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Manier van informeren dat PWA installeerbaar is', fontweight='bold')
+plt.xticks([r + barWidth/2 for r in range(2)], ['custom A2HS', 'standaard A2HS'])
+
+plt.ylabel('Procent van gebruikers die app zou installeren', fontweight='bold')
+ 
+# Create legend & Show graphic
+plt.legend()
+plt.show()
+
--- bp-data-analysis/python/barchart_usage.py
+++ bp-data-analysis/python/barchart_usage.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 barWidth = 0.25
  
 # set height of bar
-bars1 = [0.355, 0.007]
-bars2 = [1, 0.25]
-
+bars1 = [0.007, 0.5] #IOS data
+bars2 = [0.007, 0.875] #Android data
 
 # Set position of bar on X axis
 r1 = np.arange(2)
@@ -18,17 +17,11 @@ r2 = [x + barWidth for x in r1]
 plt.bar(r1, bars1, color='#FF5376', width=barWidth, edgecolor='white', label='IOS')
 plt.bar(r2, bars2, color='#242154', width=barWidth, edgecolor='white', label='Android')
  
-# Add labels to the bars
-# todooo
-
-print(r1)
-
 # Add xticks on the middle of the group bars
-plt.xlabel('Manier van informeren dat PWA installeerbaar is', fontweight='bold')
-plt.xticks([r + barWidth/2 for r in range(2)], ['custom A2HS', 'standaard A2HS'])
+plt.xlabel('Gebruiker weet wat de voordelen van een PWA zijn', fontweight='bold')
+plt.xticks([r + barWidth/2 for r in range(2)], ['nee', 'ja'])
 
-
-plt.ylabel('Procent van gebruikers die app zou installeren', fontweight='bold')
+plt.ylabel('Procent van de gebruikers die een PWA boven een native app zou kiezen', fontweight='bold')
  
 # Create legend & Show graphic
 plt.legend()
